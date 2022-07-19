3903c11d686f34ccff76f2abaa7ed7248450ccbb
Abhishek <you@example.com>
2022-07-19 12:38:36 +0530

    [Abhishek]: Employee Wage Computation Program

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.bridgeLabz.empWage;
+
+public class Main {
+
+	public static void main(String[] args) {
+System.out.println("Welcome to employee wage computation program");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
